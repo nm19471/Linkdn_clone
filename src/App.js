0feacbd0aadee0f2342c6cedfa130194bcbacf5a
Nikhil MOdak <nm19471@gmail.com>
2023-02-08 08:45:44 +0530

    Deploying my Linkdn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import {auth} from "./firebase";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getUserAuth } from "./store/user";
 
-function App() {
+
+const App=({store})=> {
+  const user = useSelector(getUserAuth());
+  
+  // useEffect(() => {
+  //   const unsubscribe = store.subscribe(() => {});
+
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+      {user ? 
+        (<Route path="/" element={<><Home/><Header/></>}/>)
+      : 
+        (<Route path="/" element={<Login/>}/>)
+      }
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBknc-BNmztZFzvSctB5VWxEMtdR4ixQHI",
+  authDomain: "linkdn-clone-28f4d.firebaseapp.com",
+  projectId: "linkdn-clone-28f4d",
+  storageBucket: "linkdn-clone-28f4d.appspot.com",
+  messagingSenderId: "230249221193",
+  appId: "1:230249221193:web:f5d701d2b944da000f0398",
+  measurementId: "G-8XVYYDJVK8"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
